48c93f51ebe8aedcdcc760d3eb9675f7d0ce2b3b
Olegium <shatroffoleg@gmail.com>
2025-03-17 13:38:21 +0300

    Добавлены комментарии в код

--- dd/hello_world.cpp
+++ dd/hello_world.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
-
+// Основная функция программы
 int main() {
     string name;
-    std::cout << "Введите ваше имя: ";
+    std::cout << "Введите ваше имя: ";// Вывод сообщения
     std::cin >> name;
-    std::cout << "Hello world from " << name << "!" << endl;
+    std::cout << "Hello world from " << name << "!" << endl;// Вывод сообщения
     return 0;
 }
